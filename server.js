0e9af36490ec1346734928f991f453b41ad3d5c1
Jason <jayjscotto@gmail.com>
2020-01-20 11:19:58 -0500

    fixing server middleware

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const path = require('path');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 8080;
 const app = express();
+const routes = require('./routes');
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/mern",
@@ -16,15 +24,10 @@ mongoose.connect(
   }
 );
 
+app.use(routes);
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.listen(port, () => {
+  console.log(`Sever listening on ${port}`);
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+
