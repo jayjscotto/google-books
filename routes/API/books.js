1b355fe5069f5331be6caa28380e1f6bea4c2cac
Jason <jayjscotto@gmail.com>
2020-01-20 14:01:45 -0500

    updating google books API route

--- routes/API/books.js
+++ routes/API/books.js
@@ -4,22 +4,27 @@ const controller = require('../../controller/booksController');
 
 // Routes matching with '/api/books'
 // reference controller methods to find data
-router.route('/')
-    .get(controller.findAll)
-    .post(controller.create);
+router
+  .route('/')
+  .get(controller.findAll)
+  .post(controller.create);
 
 // Routes matching with '/api/books/:id
 // reference controller methods to find data
-router.route('/:id')
-    .get(controller.findById)
-    .put(controller.update)
-    .delete(controller.remove);
+router
+  .route('/:id')
+  .get(controller.findById)
+  .put(controller.update)
+  .delete(controller.remove);
 
 router.get('/search-boooks', (req, res) => {
-    const query = `https://www.googleapis.com/books/v1/volumes?q=${req.body.query}&key:${process.env.API_KEY}`
-    
-    axios.get(query).then(response => console.log(response))
-})
+  const query = `https://www.googleapis.com/books/v1/volumes?q=${req.body.query}&key:${process.env.API_KEY}`;
 
+  // take search params and send data to front end in JSON from google books API
+  axios
+    .get(query)
+    .then(response => res.json(response))
+    .catch(err => res.status(418).send);
+});
 
 module.exports = router;
