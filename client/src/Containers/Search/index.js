55175f30fa43cdb1af78ff39ef7f8a238a89491c
Jason <jayjscotto@gmail.com>
2020-01-21 09:47:39 -0500

    fixed google books API get request

--- client/src/Containers/Search/index.js
+++ client/src/Containers/Search/index.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import Button from '../../Components/Button';
 import SearchResults from '../../Containers/SearchResults';
-import './search.css'
-import API from '../../Utils/externalBooks'
+import './search.css';
+import API from '../../Utils/externalBooks';
 
 class Search extends React.Component {
   state = {
-      value: '',
-      searchBooks: [],
-
+    value: '',
+    searchBooks: []
   };
 
-  googleSearch = (searchTerm) => {
-    API.searchGoogleBooks(searchTerm)
-    .then(response => {
-      const books = response.data.items;
-      books.map(book => {
-        const searchBook = {
-          title: book.volumeInfo.title,
-          authors: book.volumeInfo.authors,
-          description: book.volumeInfo.description,
-          image: book.volumeInfo.imageLinks.thumbnail,
-          link: book.volumeInfo.infoLink
-        }
-        console.log(searchBook);
-      })
-    });
-    this.setState({value: ''});
-  }
+  googleSearch = searchTerm => {
 
-  handleChange = (e) => {
-    return this.setState({value: e.target.value});
-  }
+    API.searchGoogleBooks(searchTerm).then(response => {
+      console.log(response)
+      this.setState({ searchBooks: response });
+    });
+  };
+
+  handleChange = e => {
+    return this.setState({ value: e.target.value });
+  };
 
   buttonValue = 'Search';
 
@@ -40,16 +29,27 @@ class Search extends React.Component {
       <div className='hero'>
         <div className='search-container'>
           <h3 className='hero-text'>Find Your Book:</h3>
-          <input className='search-input' type='text' name='search' value={this.state.value} onChange={this.handleChange}/>
-          <Button value={this.buttonValue} onClick={() => {this.googleSearch(this.state.value)}}/>
+          <input
+            className='search-input'
+            type='text'
+            name='search'
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <Button
+            value={this.buttonValue}
+            onClick={() => {
+              this.googleSearch(this.state.value);
+            }}
+          />
         </div>
-        <hr/>
-        <div className="add-container">
-        <h3 className='hero-text'>Find Your Book:</h3>
-    
-          <Button value={this.value}/>
+        <hr />
+        <div className='add-container'>
+          <h3 className='hero-text'>Find Your Book:</h3>
+
+          <Button value={this.value} />
         </div>
-        <SearchResults books={this.state.searchBooks}/>
+        <SearchResults books={this.state.searchBooks} />
       </div>
     );
   }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const APIroutes = require('./API');
 const axios = require('axios');
+const controller = require('../controller/bookController');
 
 router.use('/api', APIroutes);
 
@@ -10,8 +11,9 @@ router.get('/search-books/:book', (req, res) => {
   // take search params and send data to front end in JSON from google books API
   axios
     .get(query)
-    .then(response =>{
-      return res.json(response.data)
+    .then(response => {
+      const books = response.data.items.map(({volumeInfo}) => volumeInfo);
+      res.json(books)
     })
     .catch(err => res.status(418).send);
 });
@@ -19,7 +21,6 @@ router.get('/search-books/:book', (req, res) => {
 // send the React application if API routes are not hit
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}); 
-
+});
 
 module.exports = router;
